20c84f286e7cd78ef35f14baf130430b6b7e1c79
Diogo Marins <diogodsfmarins@gmail.com>
2020-01-10 00:05:02 -0300

    definindo os estilos globais

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,124 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <main id="main">
+      <transition mode="out-in">
+        <router-view />
+      </transition>
+    </main>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0px;
+  margin: 0px;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "gotham-book", "Trebuchet MS", "Helvetica Neue", Arial, Helvetica,
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #01366c;
+}
+
+a {
+  color: #01366c;
+  text-decoration: none;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #01366c;
+  border-radius: 4px;
+  color: #fff;
   text-align: center;
-  color: #2c3e50;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
 }
 
-#nav {
-  padding: 30px;
+.btn:hover {
+  background: #008fd0;
+  transform: scale(1.1);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.btn-disabled,
+.btn-disabled:hover {
+  background: #bbc;
+  transform: scale(1);
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+#main {
+  flex: 1;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"],
+textarea {
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all 0.3s;
+  font-size: 1rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  margin-bottom: 15px;
+  width: 100%;
+}
+
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #01366c;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter {
+  transform: translate3d(0, -20px, 0);
+}
+
+.v-leave-to {
+  transform: translate3d(0, 20px, 0);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
 }
 </style>
